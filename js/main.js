1c5bdc55f0ba09ddd24920a4c7ace7aab8e1f66b
zonny117 <wojn1208@naver.com>
2021-07-21 00:40:48 +0900

    test

--- js/main.js
+++ js/main.js
@@ -7,9 +7,12 @@
 // 모바일 웹 주소창 숨기기
 window.addEventListener('load', function () {
     // body의 height를 살짝 늘리는 코드
-    document.body.style.height = (document.documentElement.clientHeight + 100) + "%";
-    // scroll를 제어 하는 코드
-    setTimeout(scrollTo, 0, 0, 1);
+    if(document.documentElement.clientWidth<= 760){
+        document.body.style.height = (document.documentElement.clientHeight + 5) + "px";
+    
+        // scroll를 제어 하는 코드
+        setTimeout(scrollTo, 0, 0, 1);
+    }
 }, false);
 
 
