3662e28e66c89d1b5e3b1c94718ba88ecf40e5c7
zonny117 <wojn1208@naver.com>
2021-07-20 23:18:45 +0900

    test

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@
 // 그라데이션 트랜지션 기능은 현재 구현 불가능
 
 // 모바일 웹 주소창 숨기기
-window.addEventListener('load', function () {
-    // body의 height를 살짝 늘리는 코드
-    document.body.style.height = (document.documentElement.clientHeight + 5) + 'px';
-    // scroll를 제어 하는 코드
-    setTimeout(scrollTo, 0, 0, 1);
-}, false);
+// window.addEventListener('load', function () {
+//     // body의 height를 살짝 늘리는 코드
+//     document.body.style.height = (document.documentElement.clientHeight + 100) + "%";
+//     // scroll를 제어 하는 코드
+//     setTimeout(scrollTo, 0, 0, 1);
+// }, false);
 
 
 $(function () {
